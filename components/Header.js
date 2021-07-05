bd36d4404ee7972fdc38e2a480a6caff95a084ec
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2021-07-06 00:45:44 +0300

    Started New Portfolio Project

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Header.module.css'
+
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            
+            <h1 className={styles.name}>Karim Fawaz </h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ components/Nav.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Nav.module.css'
+
+const Nav = () => {
+    return (
+        <nav className={styles.nav}>
+           NAVBAR
+        </nav>
+    )
+}
+
+export default Nav
