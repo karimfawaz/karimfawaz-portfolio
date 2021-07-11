bfc33cbf6290fc38293791eb9a4e1d0f8deefd5b
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-07-11 19:14:50 +0300

    Working on header

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,12 @@ const Header = () => {
     return (
         <header className={styles.header}>
 
-            <h1 className={styles.name}>KARIM FAWAZ </h1>
-            <img className={` ${styles.code}  ${styles.smallIcon}`} src='/images/code.png' />
-            <img className={` ${styles.bitcoin}  ${styles.smallIcon}`} src='/images/bitcoin.png' />
-            <img className={` ${styles.cloud}  ${styles.smallIcon}`} src='/images/cloud.png' />
+            <h1 className={`${styles.name} ${styles.first}`}>KARIM</h1>
+            <h1 className={`${styles.name} ${styles.last}`}>FAWAZ</h1>
+            <img className={` ${styles.code}  ${styles.smallIcon} ${styles.scale}`} src='/images/code.png' />
+            <img className={` ${styles.bitcoin}  ${styles.smallIcon} ${styles.spin}`} src='/images/bitcoin.png' />
+            <img className={` ${styles.cloud}  ${styles.smallIcon} ${styles.scale}`} src='/images/cloud.png' />
+            <img className={` ${styles.check}  ${styles.smallIcon} ${styles.spin}`} src='/images/check.png' />
 
         </header>
     )
