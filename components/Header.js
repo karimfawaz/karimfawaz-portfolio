ec03980fe2b1c7672f2be8cb79953c4c1daf2b7a
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-07-11 13:37:43 +0300

    started working on header

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,12 @@ import styles from '../styles/Header.module.css'
 const Header = () => {
     return (
         <header className={styles.header}>
-            
+
             <h1 className={styles.name}>KARIM FAWAZ </h1>
-            <img scr="code.png"/>
+            <img className={` ${styles.code}  ${styles.smallIcon}`} src='/images/code.png' />
+            <img className={` ${styles.bitcoin}  ${styles.smallIcon}`} src='/images/bitcoin.png' />
+            <img className={` ${styles.cloud}  ${styles.smallIcon}`} src='/images/cloud.png' />
+
         </header>
     )
 }
