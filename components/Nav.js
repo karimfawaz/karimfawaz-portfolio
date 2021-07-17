109e4b2054edbf108e5a840cbfabd8fd92b1442c
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2021-07-17 20:07:40 +0300

    Started Working on Navbar SVGs

--- components/Nav.js
+++ components/Nav.js
@@ -5,19 +5,23 @@ const Nav = () => {
         <nav className={styles.nav}>
             <ul className={styles.navbarNav}>
                 <li className={styles.logo}>
-                    <a href="#" className={styles.navLink}>
-                    <span className={styles.linkText}>
+                    <a target="_blank" href="docs.google.com/document/d/13_zOmZkl9dbCe95k1qPlEF38IczTEu7tnUK5Wgu3hgQ" className={styles.navLink}>
+                        <span className={styles.linkText}>
                             Karim Fawaz
                         </span>
 
+                        <object className={` ${styles.logoSVG}  +  ${styles.svg}`} data="/arrows.svg" />
                     </a>
 
                 </li>
                 <li className={styles.navItem}>
-                    <a href="#" className={styles.navLink}>
+                    <a href="/" className={styles.navLink}>
+                        <object className={styles.svg} data="/home.svg" />
                         <span className={styles.linkText}>
                             TEST
                         </span>
+
+
                     </a>
                 </li>
 
