1bc992c319fab69e1a6bb997e0bd0c9060ea8f11
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2021-09-21 00:00:35 +0300

    Started working on experience section

--- components/Experience.js
+++ components/Experience.js
@@ -4,15 +4,15 @@ const Experience = () => {
     return (
         <section className={styles.expSection}>
             <div className={styles.sectionHeader}>
-                <img className={styles.headerImg} src='/images/network.png' />
+                <img className={styles.headerImg} src='/images/building.png' />
                 <h3 className={styles.headerTitle}>Experience</h3>
             </div>
 
             <div className={styles.expList}>
                 <div className={styles.expcard}>
                     <div className={styles.cardHeader}>
-                        <img className={styles.cardHeaderimg} src='/images/browser.png' />
-                        <h5 className={styles.cardHeadertitle}>Web Development</h5>
+                        <img className={styles.cardHeaderimg} src='/images/citronniers.png' />
+                        <h5 className={styles.cardHeadertitle}>Garderie Les Citronniers</h5>
                     </div>
                     <div className={styles.cardDetails}>
                         <p>Hey there I use HTML CSS and JS to build websites </p>
@@ -21,8 +21,8 @@ const Experience = () => {
                 </div>
                 <div className={styles.expcard}>
                     <div className={styles.cardHeader}>
-                        <img className={styles.cardHeaderimg} src='/images/mobile.png' />
-                        <h5 className={styles.cardHeadertitle}>Mobile App Development</h5>
+                        <img className={styles.cardHeaderimg} src='/images/lau.jfif' />
+                        <h5 className={styles.cardHeadertitle}>Lebanese American University</h5>
                     </div>
                     <div className={styles.cardDetails}>
                         <p>Hey there I use HTML CSS and JS to build websites </p>
@@ -31,8 +31,8 @@ const Experience = () => {
                 </div>
                 <div className={styles.expcard}>
                     <div className={styles.cardHeader}>
-                        <img className={styles.cardHeaderimg} src='/images/ufo.png' />
-                        <h5 className={styles.cardHeadertitle}>Game Development</h5>
+                        <img className={styles.cardHeaderimg} src='/images/cme.jfif' />
+                        <h5 className={styles.cardHeadertitle}>CME Offshore Sal.</h5>
                     </div>
                     <div className={styles.cardDetails}>
                         <p>Hey there I use HTML CSS and JS to build websites </p>
@@ -41,8 +41,26 @@ const Experience = () => {
                 </div>
                 <div className={styles.expcard}>
                     <div className={styles.cardHeader}>
-                        <img className={styles.cardHeaderimg} src='/images/settings.png' />
-                        <h5 className={styles.cardHeadertitle}>Procedural Programming</h5>
+                        <img className={styles.cardHeaderimg} src='/images/cme.jfif' />
+                        <h5 className={styles.cardHeadertitle}>CME Offshore Sal.</h5>
+                    </div>
+                    <div className={styles.cardDetails}>
+                        <p>Hey there I use HTML CSS and JS to build websites </p>
+                    </div>
+
+                </div><div className={styles.expcard}>
+                    <div className={styles.cardHeader}>
+                        <img className={styles.cardHeaderimg} src='/images/synkers.png' />
+                        <h5 className={styles.cardHeadertitle}>Ostaz (formerly Synkers)</h5>
+                    </div>
+                    <div className={styles.cardDetails}>
+                        <p>Hey there I use HTML CSS and JS to build websites </p>
+                    </div>
+
+                </div><div className={styles.expcard}>
+                    <div className={styles.cardHeader}>
+                        <img className={styles.cardHeaderimg} src='/images/aub.png' />
+                        <h5 className={styles.cardHeadertitle}>American University of Beirut</h5>
                     </div>
                     <div className={styles.cardDetails}>
                         <p>Hey there I use HTML CSS and JS to build websites </p>
