00d23541dabdf89771bac718c0c89a5f5ac81759
Karim Fawaz <karimhilalfawaz@gmail.com>
2021-10-21 09:55:24 +0100

    Updated Experience Section

--- components/Experience.js
+++ components/Experience.js
@@ -9,6 +9,22 @@ const Experience = () => {
             </div>
 
             <div className={styles.expList}>
+            <div className={styles.expcard}>
+                    <div className={styles.cardHeader}>
+                        <img className={styles.cardHeaderimg} src='/images/QMUL.png' />
+                        <h5 className={styles.cardHeadertitle}>QIncubator</h5>
+                        <h7 className={styles.cardHeaderDesc}>Entrepreneurship Programme</h7>
+                        <h7 className={styles.cardHeaderDate}>October 2021 - November 2021</h7>
+
+                    </div>
+                    <div className={styles.cardDetails}>
+                        <p>•	Elected for QMUL Enterprise’s flagship programme.
+                        <br/>•	Mentored by UK Government 2021 winner Julio Alejandro. 
+                        <br/>•	Introduced to the foundations of entrepreneurship, lean canvas development, and start-up pitching.
+                        </p>
+                    </div>
+
+                </div>
                 <div className={styles.expcard}>
                     <div className={styles.cardHeader}>
                         <img className={styles.cardHeaderimg} src='/images/citronniers.png' />
@@ -18,7 +34,7 @@ const Experience = () => {
 
                     </div>
                     <div className={styles.cardDetails}>
-                        <p>Hey there I use HTML CSS and JS to build websites </p>
+                        <p>•	Improved the nursery’s website SEO rating by 30% and made Garderie Les Citronniers the first result when searching for the best nursery in Beirut on Google after previously being featured on the 4th page of the search results. </p>
                     </div>
 
                 </div>
@@ -31,7 +47,8 @@ const Experience = () => {
 
                     </div>
                     <div className={styles.cardDetails}>
-                        <p>Hey there I use HTML CSS and JS to build websites </p>
+                        <p>•    Developed a music player, a to-do list application, and a timer application on Android Studio using Java for the final project competition.<br />•	Won 2nd Place.<br />•	Earned a 20% scholarship.
+                        </p>
                     </div>
 
                 </div>
@@ -44,7 +61,8 @@ const Experience = () => {
 
                     </div>
                     <div className={styles.cardDetails}>
-                        <p>Hey there I use HTML CSS and JS to build websites </p>
+                        <p>•	Assimilated RecyclerView in Android applications.<br />•	Supported the migration from a Java to Kotlin for the Subway bread management Android application.
+                        </p>
                     </div>
 
                 </div>
@@ -57,7 +75,11 @@ const Experience = () => {
 
                     </div>
                     <div className={styles.cardDetails}>
-                        <p>Hey there I use HTML CSS and JS to build websites </p>
+                        <p>•	Liaised with the backend team who were developing software regarding pharmaceuticals using C#.
+                        <br/>•	Collaborated with the Android and IOS teams who were developing mobile applications for the Subway supply chain.
+                        <br/>•	Participated with the hardware team who were creating a gadget to reduce electricity consumption.
+                        <br/>•	Blended with the Telekom team who showed me how they operate internationally.
+                        </p>
                     </div>
 
                 </div><div className={styles.expcard}>
@@ -66,13 +88,16 @@ const Experience = () => {
                         <h5 className={styles.cardHeadertitle}>Ostaz (formerly Synkers)</h5>
                         <h7 className={styles.cardHeaderDesc}>Start-Up Contest</h7>
                         <h7 className={styles.cardHeaderDate}>April 2018 - June 2018</h7>
-                       
+
                     </div>
                     <div className={styles.cardDetails}>
-                        <p>Hey there I use HTML CSS and JS to build websites </p>
+                        <p>•	Elected from my school to pitch my gamified learning software (using Unity2D) to teach the driving basics to younger audiences (age group: 12-18).
+                        <br/>•	Finalist for the competition.
+                        </p>
                     </div>
 
-                </div><div className={styles.expcard}>
+                </div>
+                <div className={styles.expcard}>
                     <div className={styles.cardHeader}>
                         <img className={styles.cardHeaderimg} src='/images/aub.png' />
                         <h5 className={styles.cardHeadertitle}>American University of Beirut</h5>
@@ -81,7 +106,7 @@ const Experience = () => {
 
                     </div>
                     <div className={styles.cardDetails}>
-                        <p>Hey there I use HTML CSS and JS to build websites </p>
+                        <p>•	Introduced to Robotics and won 1st place in the group competition. </p>
                     </div>
 
                 </div>
