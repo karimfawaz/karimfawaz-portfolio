3efcb038bf58e2a40c870886561735618596cca1
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2021-11-15 10:00:07 +0000

    Added image to each tech skill

--- components/TechSkills.js
+++ components/TechSkills.js
@@ -8,83 +8,83 @@ const TechSkills = () => {
             </div>
             <div className={styles.techGrid}>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/java.png'/>
                     <h2 className={styles.gridItemTitle}>Java</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/javascript.png'/>
                     <h2 className={styles.gridItemTitle}>JavaScript</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/python.png'/>
                     <h2 className={styles.gridItemTitle}>Python</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/sql.png'/>
                     <h2 className={styles.gridItemTitle}>SQL - DBMS</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/nextjs.png'/>
                     <h2 className={styles.gridItemTitle}>Next.js</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/react.png'/>
                     <h2 className={styles.gridItemTitle}>React.js</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/flutter.png'/>
                     <h2 className={styles.gridItemTitle}>Flutter</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/dart.png'/>
                     <h2 className={styles.gridItemTitle}>Dart</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/kotlin.png'/>
                     <h2 className={styles.gridItemTitle}>Kotlin</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/csharp.png'/>
                     <h2 className={styles.gridItemTitle}>C#</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/unity.png'/>
                     <h2 className={styles.gridItemTitle}>Unit2D</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/css.png'/>
                     <h2 className={styles.gridItemTitle}>CSS</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/html.png'/>
                     <h2 className={styles.gridItemTitle}>HTML</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/php.svg'/>
                     <h2 className={styles.gridItemTitle}>PHP</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/framer.jpg'/>
                     <h2 className={styles.gridItemTitle}>Framer Motion</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/github.jpg'/>
                     <h2 className={styles.gridItemTitle}>GitHub</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
-                    <h2 className={styles.gridItemTitle}>PHP</h2>
+                    <img className={styles.gridItemImg} src='/images/shopify.jpg'/>
+                    <h2 className={styles.gridItemTitle}>Shopify</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
-                    <h2 className={styles.gridItemTitle}>WordPress (and it's plugins)</h2>
+                    <img className={styles.gridItemImg} src='/images/wordpress.png'/>
+                    <h2 className={styles.gridItemTitle}>WordPress</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/firebase.jpg'/>
                     <h2 className={styles.gridItemTitle}>Firebase</h2>
                 </div>
                 <div className={styles.gridItem}>
-                    <img className={styles.gridItemImg}/>
+                    <img className={styles.gridItemImg} src='/images/mips.png'/>
                     <h2 className={styles.gridItemTitle}>MIPS Assembly</h2>
                 </div>
             </div>
