dfb50861fe1f41d9d467ded0934c4c68d311e1c9
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2022-01-17 23:49:50 +0000

    Fixed Header: Better Design Decision

--- components/Header.js
+++ components/Header.js
@@ -4,14 +4,47 @@ import styles from '../styles/Header.module.css'
 const Header = () => {
     return (
         <header className={styles.header}>
+            <table className={styles.table}>
+                <tr>
+                    <td colSpan="2">
+                        <h1 className={`${styles.name} ${styles.first}`}>KARIM</h1>
+                    </td>
+                    <td>
+                        <img className={`  ${styles.smallIcon} ${styles.scale}`} src='/images/cloud.png' />
 
-            <h1 className={`${styles.name} ${styles.first}`}>KARIM</h1>
-            <h1 className={`${styles.name} ${styles.last}`}>FAWAZ</h1>
-            <img className={` ${styles.code}  ${styles.smallIcon} ${styles.scale}`} src='/images/code.png' />
-            <img className={` ${styles.bitcoin}  ${styles.smallIcon} ${styles.spin}`} src='/images/bitcoin.png' />
-            <img className={` ${styles.cloud}  ${styles.smallIcon} ${styles.scale}`} src='/images/cloud.png' />
-            <img className={` ${styles.check}  ${styles.smallIcon} ${styles.spin}`} src='/images/check.png' />
-            <img className={` ${styles.browser}  ${styles.smallIcon} ${styles.scale}`} src='/images/chart.png' />
+                    </td>
+                    <td>
+
+                    </td>
+                    <td className={styles.alignCenter}>
+                        <img className={`  ${styles.smallIcon} ${styles.spin}`} src='/images/check.png' />
+
+
+                    </td>
+                </tr>
+
+
+
+                <tr>
+                    <td >
+                        <img className={`  ${styles.smallIcon} ${styles.scale}`} src='/images/code.png' />
+                    </td>
+
+                    <td className={styles.alignEnd}>
+                        <img className={`  ${styles.smallIcon} ${styles.spin}`} src='/images/bitcoin.png' />
+                    </td>
+
+                    <td >
+                        <h1 className={`${styles.name} ${styles.last}`}>FAWAZ</h1>
+                    </td>
+
+                    <td >
+                        <img className={` ${styles.smallIcon} ${styles.scale}`} src='/images/chart.png' />
+                    </td>
+
+
+                </tr>
+            </table>
 
         </header>
     )
