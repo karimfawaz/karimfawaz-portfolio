6a2e9068d9d1572a28b2ec8515d721016f92f182
Karim Fawaz <karimhilalfawaz@gmail.com>
2022-03-02 21:25:32 +0000

    Updated TechSkills and Education

--- components/Education.js
+++ components/Education.js
@@ -8,32 +8,15 @@ const Education = () => {
                 <h3 className={styles.headerTitle}>Education</h3>
             </div>
 
-            {/* <table className={styles.table}>
-
-                <tr>
-                <td> <img className={styles.eduImg} src='/images/cpf.png'/></td>
-
-                    <td>2005 <span className={styles.span}>-</span> 2020</td>
-                    <td>College Protestant Francais de Beyrouth</td>
-                    <td>French Baccalaureate | Scientific Path, Specialised in Maths</td>
-                </tr>
-                <tr>
-                    <td> <img className={styles.eduImg} src='/images/qmul.png'/></td>
-                    <td>2020 <span className={styles.span}>-</span> 2023</td>
-                    <td>Queen Mary <span className={styles.span}>,</span> University of London</td>
-                    <td>Computer Science <span className={styles.span}>-</span> Bachelor of Science | Grade: A (87%)</td>
-                </tr>
-
-            </table> */}
-            <div className={styles.eduDiv}>
+         <div className={styles.eduDiv}>
                 <div className={styles.eduCard}>
                     <img className={styles.eduImg} src='/images/cpf.png' />
                     <div className={styles.eduDetails}>
                         <h3 className={styles.eduTitle}>
-                            College Protestant Francais de Beyrouth
+                            College Protestant Francais
                         </h3>
                         <p className={styles.eduText}>
-                        French Baccalaureate - Scientific Path, Emphasis in Mathematics
+                        French Baccalaureate<br/>Scientific Path, Emphasis in Mathematics
                         </p>
                     </div>
 
@@ -42,10 +25,10 @@ const Education = () => {
                     <img className={styles.eduImg} src='/images/qmul.png' />
                     <div className={styles.eduDetails}>
                         <h3 className={styles.eduTitle}>
-                            College Protestant Francais de Beyrouth
+                            Queen Mary University of London
                         </h3>
                         <p className={styles.eduText}>
-                        French Baccalaureate - Scientific Path, Emphasis in Mathematics
+                        Computer Science, Bachelor of Sciences
                         </p>
                     </div>
 
